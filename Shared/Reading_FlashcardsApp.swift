66f6b0f22a1a40f40571a5f3db4a6d7aa19c1437
Cassie Murphy <c.murphy@techsmith.com>
2022-05-21 14:20:23 -0400

    Create project

---
+++ Shared/Reading_FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reading_FlashcardsApp.swift
+//  Shared
+//
+//  Created by Murphy, Cassie on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Reading_FlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
