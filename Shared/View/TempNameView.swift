c2120624183982dc8ba9aab02f39ea58456e1085
Cassie Murphy <c.murphy@techsmith.com>
2022-06-19 10:06:19 -0400

    Rebase and adjust

--- Shared/View/TempNameView.swift
+++ Shared/View/TempNameView.swift
@@ -7,32 +7,47 @@
 
 import SwiftUI
 
-struct FlashCardsView: View {
+struct TempNameView: View {
+   @State private var flashcardVM = FlashcardViewModel()
+   
+   var studyListArray = ["hat", "cat", "bat", "flat"]
+   // FIXME: Use this as base for multiple views with this style screen. Refactor name to meet these needs
    
     var body: some View {
+       
        GeometryReader { geometry in
           VStack {
              Image("LimeMelt")
                 .resizable()
 //                .scaledToFit()
                 .frame(width: geometry.size.width, alignment: .center)
-             CardView(word: WordManager(word: "hat",
-                                        definition: "Item worn on head"))
-                .withOverlayStyle(bgColor: Color("MonsterBase"),
-                                  height: geometry.size.height * 0.4,
-                                  offsetY: geometry.size.height * 0.01)
-                                 // FIXME: offset adjustments need checking
+             ZStack {
+                ForEach(flashcardVM.words, id: \.id) { word in
+                   FlashcardView(word: word)
+                      .background(Color("MonsterBase"))
+                      .clipShape(RoundedRectangle(cornerRadius: 25))
+                }
+             }
+//             FlashcardView(word: WordManager(word: "hat",
+//                                        definition: "Item worn on head"))
+//                .withOverlayStyle(bgColor: Color("MonsterBase"),
+//                                  height: geometry.size.height * 0.4,
+//                                  offsetY: geometry.size.height * 0.01)
+//                                 // FIXME: offset adjustments need checking
           }.background(Color("MonsterLime"))
-             .ignoresSafeArea()
+             .onAppear(perform: {
+                flashcardVM.addStudyList(studyList: studyListArray)
+             })
        }
     }
 }
 
-// FIXME: Issues with landscape iphone view. Keep portrait or make offset changes based on orientaion.
-
+//
+//// FIXME: Issues with landscape iphone view. Keep portrait or make offset changes based on orientaion.
+//
 struct FlashCardView_Previews: PreviewProvider {
     static var previews: some View {
-       FlashCardsView()
+       TempNameView()
     }
 }
 
